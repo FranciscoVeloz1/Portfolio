7bb4bb96f37d016e1e6a695262d5248cb46c5cba
FranciscoVeloz <franciscoveloz245@gmail.com>
2022-02-15 12:25:51 -0600

    Agregamos la tabla de experience

--- src/pages/AdminExperience/Experience.jsx
+++ src/pages/AdminExperience/Experience.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Table from "@components/global/Table";
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  const tableOptions = {
+    data: [
+      {
+        id: "1",
+        job: "",
+        description: "",
+        image: "",
+        tech: "",
+      },
+    ],
+    title: "Experience",
+    headers: ["ID", "Job", "Description", "Image", "Technology"],
+    addLink: "/admin/experience/add",
+    editLink: "/admin/experience/edit",
+    delete: () => {},
+  };
 
-export default Experience
+  return (
+    <div className="admin-projects-container">
+      <Table o={tableOptions} />
+    </div>
+  );
+};
+
+export default Experience;
